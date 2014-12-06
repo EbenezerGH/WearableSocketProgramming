850ceed3783cad4ac5bb1a5111587ccecec802bc
Ebenezer Ackon <Ebenezer.Ackon@viacommix.com>
2014-12-06 18:10:08 -0500

    Toasting on Mobile Device

--- mobile/src/main/java/wearable/com/wearablehackathon/ListenerService.java
+++ mobile/src/main/java/wearable/com/wearablehackathon/ListenerService.java
@@ -9,18 +9,14 @@ import com.google.android.gms.wearable.WearableListenerService;
  * Created by ackoneb on 12/6/14.
  */
 public class ListenerService extends WearableListenerService {
-    MainActivity activity;
-
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         GlobalMessage globalMessage = null;
         if (messageEvent.getPath().equals("/message_path")) {
             final String message = new String(messageEvent.getData());
-            //  Log.v("switching", "Message path received on watch is: " + messageEvent.getPath());
-            //Log.v("switching", "Message received on Device is: " + message);
-            globalMessage.setMyMessage(message);
 
+            globalMessage.setMyMessage(message);
             Toast.makeText(getApplicationContext(), message,
                     Toast.LENGTH_LONG).show();
 
